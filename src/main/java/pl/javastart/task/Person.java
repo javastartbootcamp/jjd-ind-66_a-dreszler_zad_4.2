062aaa30ef084a8b06215ba1d33dcb7b7c1ba0af
Aleksander Dreszler <olek.d93@gmail.com>
2023-02-15 17:58:04 +0100

    Wykonano zadanie.

---
+++ src/main/java/pl/javastart/task/Person.java
@@ -0,0 +1,11 @@
+package pl.javastart.task;
+
+class Person {
+    String firstName;
+    String lastName;
+
+    Person(String firstName, String secondName) {
+        this.firstName = firstName;
+        this.lastName = secondName;
+    }
+}
